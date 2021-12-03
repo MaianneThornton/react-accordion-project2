9ea1b05a0f58f79f57d96d1e0f5d34610e5367d2
MaianneThornton <maianne.thornton@gmail.com>
2021-12-03 14:21:14 -0500

    hid the p elements by default as well as added functionality to buttons. initial version of project is complete.

--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 //accessing the properties from the questions array and objects
-const Question = ({title, info}) => {
-  return (<article className="question">
-    <header>
-      <h4>{title}</h4>
-      <button className="btn">
-        'btn'
-      </button>
-    </header>
-    <p>{info}</p>
-  </article>
-)};
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() =>
+        //setting the setShowInfo parameter to the opposite of whatever showInfo's current state is onClick 
+        { setShowInfo(!showInfo) }}>
+          {/* toggles the icons via a ternary */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* hides the p element by default: if showInfo is true then display the p element */}
+      {showInfo && <p>{info}</p>}
+
+    </article>
+  )
+};
 
 export default Question;
