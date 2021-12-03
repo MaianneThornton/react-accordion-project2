7fac027e3acc16901afbf52e09a34e7c8ce1042a
MaianneThornton <maianne.thornton@gmail.com>
2021-12-03 14:21:55 -0500

    formatted, initial version of project is complete

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import SingleQuestion from './Question';
 function App() {
   // sets the array of questions from the data.js file as the useState value
   const [questions, setQuestions] = useState(data);
-  return <main>
-    <div className="container">
-      <h3>questions and answers about login</h3>
-      <section className="info">
-        {
-          //iterating the list of questions from question.js via the SingleQuestion component and then spreading all the properties beginning with id.
-          questions.map((question) => {
-            return <SingleQuestion key={question.id}{...question}/>;
-          })};
-        
-      </section>
-    </div>
-  </main>;
-}
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {
+            //iterating the list of questions from question.js via the SingleQuestion component and then spreading all the properties beginning with id.
+            questions.map((question) => {
+              return (<SingleQuestion key={question.id}{...question} />);
+            })};
+
+        </section>
+      </div>
+    </main>
+  )
+};
 
 export default App;
