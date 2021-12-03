5998c9501415149007751f36080591acb56fd574
MaianneThornton <maianne.thornton@gmail.com>
2021-12-03 13:59:58 -0500

    set the questions array as the useState value as well as returned the iterated array in the info section.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  // sets the array of questions from the data.js file as the useState value
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {
+          //iterating the list of questions from question.js via the SingleQuestion component and then spreading all the properties beginning with id.
+          questions.map((question) => {
+            return <SingleQuestion key={question.id}{...question}/>;
+          })};
+        
+      </section>
+    </div>
+  </main>;
 }
 
 export default App;
